e502f11f2828e45138a967da6af12ef49cccf5a4
EliasLima <eliasbarrosl@gmail.com>
2021-11-04 12:53:25 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -1,13 +1,14 @@
 package com.devsuperior.dsvendas.services;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsvendas.dto.SaleDTO;
 import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 
